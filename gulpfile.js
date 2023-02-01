5516754c6579a57f0cee9a4c50cce5642565717b
Kwang <punyarit.thi@hlabconsulting.com>
2023-02-01 11:21:00 +0700

    set babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 const gulp = require('gulp');
 const uglify = require('gulp-uglify');
+const babel = require('gulp-babel');
 
 gulp.task('compress', function () {
-  return gulp.src('build/**/*.js').pipe(uglify()).pipe(gulp.dest('cx'));
+  return gulp
+    .src('build/**/*.js')
+    .pipe(babel())
+    .pipe(uglify())
+    .pipe(gulp.dest('cx'));
 });
