37517a0d4a335d21ca6a6108c7cfbcfaaa6c7323
Kwang <punyarit.thi@hlabconsulting.com>
2023-02-07 13:47:44 +0700

    fix logic popover position display

--- src/components/modal/state/popover.state.ts
+++ src/components/modal/state/popover.state.ts
@@ -11,25 +11,11 @@ const positionReverseOverScreen = {
   right: 'left',
 } as const;
 
-function checkPositionType(
-  position: PopoverPositionType
-): (resizeEntry: ResizeObserverEntry) => number {
-  const [positionType] = position.split('-') as Array<keyof typeof positionReverseOverScreen>;
-  return (resizeValue: ResizeObserverEntry) => {
-    console.log('popover.state.js |positionType| = ', positionType);
-    console.log('popover.state.js |resizeValue| = ', resizeValue);
-    return 0;
-  };
-}
-
-function checkReverseOverScreen(position: PopoverPositionType) {
-  const checkOverScreen = checkPositionType(position);
-  return (e: ResizeObserverEntry) => checkOverScreen(e);
-}
+const sidePopoverAppear = ['center', 'left', 'right', 'top', 'bottom'] as const;
 
 function calc({ x, y }: { x: number; y: number }) {
-  const xValue = Math.abs(Math.floor(x));
-  const yValue = Math.abs(Math.floor(y));
+  const xValue = Math.floor(x);
+  const yValue = Math.floor(y);
   return {
     x: xValue,
     y: yValue,
@@ -56,7 +42,7 @@ export class PopoverState {
     this.setOpacity('0');
     this.setContentInlineBlock();
     this.setPosition();
-    this.setBlurEventListener();
+    this.setFocusOutEventListener();
     this.setPopoverAppear();
     requestAnimationFrame(() => {
       this.setOpacity('1');
@@ -95,14 +81,10 @@ export class PopoverState {
       this.popoverContent
     ).getResult();
 
-    this.popoverContent.style.translate = position;
+    this.popoverContent.style.translate = position!;
   }
 
-  popoverContentFocus = () => {
-    this.popoverContent.focus();
-  };
-
-  private setBlurEventListener() {
+  private setFocusOutEventListener() {
     this.popoverContent.tabIndex = 0;
     this.popoverContent.addEventListener('focusout', (e) => {
       const isInsidePopover = e.relatedTarget as HTMLElement;
@@ -157,86 +139,112 @@ class PopoverPosition {
     });
   }
   public async getResult() {
-    await this.checkPopoverRectUsed();
-    return this.getPosition();
+    await this.setPopoverRect();
+    // 📌positionResult is position that popover content will appear in screen (inject to translate)
+    // 📌The Summary is the x / y position to place the popover.
+    const positionResult = this.getPosition();
+
+    // 📌position is top left right bottom
+    // 📌side is left center right / top center bottom
+    const [position, side] = this.position.split('-') as [
+      keyof typeof positionReverseOverScreen,
+      typeof sidePopoverAppear[number]
+    ];
+    let isOverScreen: boolean = false;
+    if (position === 'right') {
+      isOverScreen = this.popoverRect!.width + positionResult.x > resizeEntry!.contentRect.width;
+    } else if (position === 'left') {
+      isOverScreen = positionResult.x < 0;
+    } else if (position === 'bottom') {
+      isOverScreen = this.popoverRect!.height + positionResult.y > resizeEntry!.contentRect.height;
+    } else if (position === 'top') {
+      isOverScreen = positionResult.y < 0;
+    }
+
+    // 📌true = over screen
+    if (isOverScreen) {
+      // 📌reverse position
+      this.position = `${positionReverseOverScreen[position]}-${side}` as PopoverPositionType;
+      return this.getPosition().translate;
+    }
+    return positionResult.translate;
   }
 
-  private async checkPopoverRectUsed() {
-    // if (!this.positionUsedPopoverRect.has(this.position) && this.popoverRect) return;
+  private async setPopoverRect() {
     this.popoverRect = await this.getPopoverRect();
   }
 
-  private getPosition(): string {
+  private getPosition(): ReturnType<typeof calc> {
     switch (this.position) {
       default:
       case 'bottom-left':
-        return calc({ x: this.hostRect.left, y: this.hostRect.bottom }).translate;
+        return calc({ x: this.hostRect.left, y: this.hostRect.bottom });
 
       case 'bottom-center':
         return calc({
           x: this.hostRect.width / 2 + this.hostRect.left - this.popoverRect!.width / 2,
           y: this.hostRect.bottom,
-        }).translate;
+        });
 
       case 'bottom-right':
         return calc({
           x: this.hostRect.right - this.popoverRect!.width,
           y: this.hostRect.bottom,
-        }).translate;
+        });
 
       case 'top-left':
         return calc({
           x: this.hostRect.left,
           y: this.hostRect.top - this.popoverRect!.height,
-        }).translate;
+        });
 
       case 'top-center':
         return calc({
           x: this.hostRect.width / 2 + this.hostRect.left - this.popoverRect!.width / 2,
           y: this.hostRect.top - this.popoverRect!.height,
-        }).translate;
+        });
 
       case 'top-right':
         return calc({
           x: this.hostRect.right - this.popoverRect!.width,
           y: this.hostRect.top - this.popoverRect!.height,
-        }).translate;
+        });
 
       case 'left-top':
         return calc({
           x: this.hostRect.left - this.popoverRect!.width,
           y: this.hostRect.top,
-        }).translate;
+        });
 
       case 'left-center':
         return calc({
           x: this.hostRect.left - this.popoverRect!.width,
           y: this.hostRect.top + this.hostRect.height / 2 - this.popoverRect!.height / 2,
-        }).translate;
+        });
 
       case 'left-bottom':
         return calc({
           x: this.hostRect.left - this.popoverRect!.width,
-          y: this.popoverRect!.height - this.hostRect.bottom,
-        }).translate;
+          y: this.hostRect.bottom - this.popoverRect!.height,
+        });
 
       case 'right-top':
         return calc({
           x: this.hostRect.right,
           y: this.hostRect.top,
-        }).translate;
+        });
 
       case 'right-center':
         return calc({
           x: this.hostRect.right,
           y: this.hostRect.top + this.hostRect.height / 2 - this.popoverRect!.height / 2,
-        }).translate;
+        });
 
       case 'right-bottom':
         return calc({
           x: this.hostRect.right,
-          y: this.popoverRect!.height - this.hostRect.bottom,
-        }).translate;
+          y: this.hostRect.bottom - this.popoverRect!.height,
+        });
     }
   }
 }
--- src/observer/resize.observer.ts
+++ src/observer/resize.observer.ts
@@ -2,5 +2,4 @@ export let resizeEntry: ResizeObserverEntry | null = null;
 
 export const resizeObserver = new ResizeObserver(([entry]) => {
   resizeEntry = entry;
-  console.log('resize.observer |resizeEntry|', resizeEntry);
 });
