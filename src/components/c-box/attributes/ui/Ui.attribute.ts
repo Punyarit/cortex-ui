96e5f1ffdf142426747225889a5136bfee1accbd
Kwang <punyarit.thi@hlabconsulting.com>
2023-03-09 16:18:59 +0700

    fix mutiple ui styles

--- src/components/c-box/attributes/ui/Ui.attribute.ts
+++ src/components/c-box/attributes/ui/Ui.attribute.ts
@@ -2,30 +2,34 @@ import { stylesMapper } from '../../styles-mapper/styles-mapper';
 
 export class UI {
   static init(box: CBox.Ref, value: string) {
-    const [uiName, styles] = value?.split(':');
-    if (!uiName || !styles) return;
+    if (!value) return;
 
-    // get styles
-    let styleText = ``;
-    for (const style of styles?.split(' ')) {
-      style &&
-        (styleText = styleText.concat(stylesMapper.get(`c-box[${style.trim()}]`) + '!important;'));
-    }
-
+    const stylesGroup = value.split(',');
+    let uiNames = '';
     // open shadow root
     const shadowRoot = box.attachShadow({ mode: 'open' });
 
     // append style
     const shadowStyle = document.createElement('style');
     shadowRoot.appendChild(shadowStyle);
-    const styleSheet = shadowRoot.styleSheets[0];
-    styleSheet.insertRule(`:host([ui='${uiName}']){${styleText}}`);
 
     // append slot
     const slot = document.createElement('slot');
     shadowRoot.appendChild(slot);
 
-    // set attribute
-    box.setAttribute('ui', uiName);
+    for (const styleInGroup of stylesGroup) {
+      const [uiName, styles] = styleInGroup?.split(':');
+      if (!uiName || !styles) return;
+      uiNames = uiNames.concat(uiName);
+      let styleText = ``;
+      for (const style of styles?.split(' ')) {
+        style && (styleText = styleText.concat(stylesMapper.get(`c-box[${style.trim()}]`) + ';'));
+      }
+      const styleSheet = shadowRoot.styleSheets[0];
+      styleSheet.insertRule(`:host([_ui~='${uiName.trim()}']){${styleText}}`, 0);
+    }
+
+    box.setAttribute('_ui', uiNames);
+    box.removeAttribute('ui');
   }
 }
--- src/helpers/ui.ts
+++
@@ -1,3 +0,0 @@
-export const ui = (styles: TemplateStringsArray) => {
-  console.log('ui.js |styles| = ', styles);
-};
