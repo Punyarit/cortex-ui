d37977358c53b2d441a9c3e63a4baf0cf8b5db56
Kwang <punyarit.thi@hlabconsulting.com>
2023-05-02 20:45:44 +0700

    remove unused code

--- src/components/c-box2/types/pseudo-classes.type.ts
+++
@@ -1,8 +0,0 @@
-import { ColorAttr } from '../../../types/colors.type';
-import { TxColorKey } from './tx-colors.type';
-
-export type HoverColorKey = `hover-${keyof ColorAttr}`;
-export type ActiveColorKey = `active-${TxColorKey}`;
-
-export type HoverColorAttr = Partial<Record<HoverColorKey, boolean>>;
-export type ActiveColorAttr = Partial<Record<ActiveColorKey, boolean>>;
